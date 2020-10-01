ba2fa495b57e4523363f9d980d8b296fe483f053
131299 <71362125+131299@users.noreply.github.com>
2020-09-30 20:18:56 -0600

    Add files via upload

---
+++ BLL/clsUsuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace BLL
+{
+    public class clsUsuarios
+    {
+        public ValidarUsuarioResult validarUsuario(string Correo, string Clave)
+        {
+            try
+            {
+                DatosDataContext db = new DatosDataContext();
+                ValidarUsuarioResult dato = db.ValidarUsuario(Correo, Clave).FirstOrDefault();
+                return dato;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
